a6819cec95ca47cabc3cd085a667106e49adff6e
sanjitha.siva <sanjitha.siva@aspiresys.com>
2024-01-23 11:53:17 +0530

    commit

--- src/app/dynamic-form/dynamic-form.component.ts
+++ src/app/dynamic-form/dynamic-form.component.ts
@@ -1,50 +1,60 @@
-import { Component, Input, OnChanges, OnInit, SimpleChanges,ChangeDetectionStrategy } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
-import {JsonForm,JsonFormConfig} from '../interface/form.interface';
+import {
+  Component,
+  Input,
+  OnChanges,
+  OnInit,
+  SimpleChanges,
+  ChangeDetectionStrategy,
+} from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { JsonForm, JsonFormConfig } from '../interface/form.interface';
 import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-dynamic-form',
   templateUrl: './dynamic-form.component.html',
-  styleUrls: ['./dynamic-form.component.css']
+  styleUrls: ['./dynamic-form.component.css'],
 })
-export class DynamicFormComponent implements OnChanges{
- 
+export class DynamicFormComponent implements OnChanges {
   @Input() myFormData!: JsonForm;
-  public myForm:FormGroup=this.formBuilder.group({})
+  public myForm: FormGroup = this.formBuilder.group({});
   public selectedValue: string | undefined;
-  constructor(private formBuilder:FormBuilder,private http: HttpClient){}
+  constructor(private formBuilder: FormBuilder, private http: HttpClient) {}
   ngOnChanges(changes: SimpleChanges) {
-    if(!changes['myFormData'].firstChange){
-      console.log("data: ",this.myFormData)
-      this.buildForm(this.myFormData?.controls)
+    if (!changes['myFormData'].firstChange) {
+      console.log('data: ', this.myFormData);
+      this.buildForm(this.myFormData?.controls);
     }
   }
   buildForm(controls: JsonFormConfig[]) {
-    console.log("build : ", controls);
-  
+    console.log('build : ', controls);
+
     for (const ele of controls) {
-      console.log("ele: ", ele);
-      this.myForm.addControl(
-        ele.name,
-        this.formBuilder.control(ele.value)
-      );
+      console.log('ele: ', ele);
+      this.myForm.addControl(ele.name, this.formBuilder.control(ele.value));
     }
-    console.log("f: ",this.myForm)
-    
+    console.log('f: ', this.myForm);
   }
 
-  onDropdownChange(event: any,name:any) {
-    console.log("e: ",event," name: ",name)
-     this.selectedValue = event.target.value;
+  onDropdownChange(event: any, name: any) {
+    console.log('e: ', event, ' name: ', name);
+    this.selectedValue = event.target.value;
     this.myForm.get(name)?.setValue(this.selectedValue);
-    if (this.selectedValue) {
-      this.buildForm(this.myFormData?.additionalDetails)
-      console.log(this.myForm)
-    } 
-    else {
+    if (this.selectedValue && name == 'Id Proof') {
+      this.buildForm(this.myFormData?.additionalDetails);
+      console.log(this.myForm);
+    } else {
       this.myForm.removeControl('additionalDetails');
     }
+  }
+
+  onOthersDropdownChange(event: any) {
+    console.log('other: ', this.myForm);
   }
 
   onSubmit() {

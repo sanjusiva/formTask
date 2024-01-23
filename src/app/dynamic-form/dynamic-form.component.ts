27837eb6ed3428a16371c8ec868b11f009969feb
sanjitha.siva <sanjitha.siva@aspiresys.com>
2024-01-23 18:14:40 +0530

    done

--- src/app/dynamic-form/dynamic-form.component.ts
+++ src/app/dynamic-form/dynamic-form.component.ts
@@ -24,6 +24,7 @@ export class DynamicFormComponent implements OnChanges {
   @Input() myFormData!: JsonForm;
   public myForm: FormGroup = this.formBuilder.group({});
   public selectedValue: string | undefined;
+  public idProof:any;
   constructor(private formBuilder: FormBuilder, private http: HttpClient) {}
   ngOnChanges(changes: SimpleChanges) {
     if (!changes['myFormData'].firstChange) {
@@ -31,8 +32,8 @@ export class DynamicFormComponent implements OnChanges {
       this.buildForm(this.myFormData?.controls);
     }
   }
-  buildForm(controls: JsonFormConfig[]) {
-    console.log('build : ', controls);
+  buildForm(controls: JsonFormConfig[],otherId?:any) {
+    console.log('build : ', controls,otherId);
 
     for (const ele of controls) {
       console.log('ele: ', ele);
@@ -40,7 +41,8 @@ export class DynamicFormComponent implements OnChanges {
         ele.validators.required?this.myForm.addControl(ele.name, this.formBuilder.control(ele.value,Validators.required)):
         this.myForm.addControl(ele.name, this.formBuilder.control(ele.value));
         console.log('Form control added:', ele.name);
-        if (ele.clg) {
+        if(otherId){
+        if (ele.clg.name===otherId && otherId!==undefined) {
           console.log('n: ', ele.clg.name);
         ele.validators.required?this.myForm.addControl(ele.clg.name, this.formBuilder.control(ele.clg.value,Validators.required)):
           this.myForm.addControl(
@@ -50,7 +52,8 @@ export class DynamicFormComponent implements OnChanges {
         console.log('Form control added:', ele.clg.name);
 
         } 
-        if (ele.school) {
+        else if (ele.school.name===otherId && otherId!==undefined) {
+          console.log('n: ', ele.school.name);
         ele.validators.required?this.myForm.addControl(ele.school.name, this.formBuilder.control(ele.school.value,Validators.required)):
           this.myForm.addControl(
             ele.school.name,
@@ -60,6 +63,7 @@ export class DynamicFormComponent implements OnChanges {
           
         }
       }
+      }
     }
     console.log('Form controls:', this.myForm.controls);
     console.log('f: ', this.myForm);
@@ -67,9 +71,13 @@ export class DynamicFormComponent implements OnChanges {
 
   onDropdownChange(event: any, name: any) {
     console.log('e: ', event.target.value, ' name: ', name);
+    console.log('prev: ', this.selectedValue,"curr: ",event.target.value);
+    if(event.target.value!==this.selectedValue && this.selectedValue!==undefined){
+      console.log("change: ",this.selectedValue)
+      this.myForm.removeControl(this.selectedValue);
+    }
     this.selectedValue = event.target.value;
     this.myForm.get(name)?.setValue(this.selectedValue);
-    console.log('ad: ', this.selectedValue);
     if (this.selectedValue) {
       if (this.selectedValue === 'aadhar') {
         console.log('a: ', this.myFormData.additionalDetails[0]['aadhar']);
@@ -79,20 +87,27 @@ export class DynamicFormComponent implements OnChanges {
         this.buildForm(this.myFormData?.additionalDetails[1]['dl']);
       } 
       else if (this.selectedValue === 'others') {
-        console.log('ingaaaaaaaaaaaaaaaaaaa');
+        console.log('ingaaaaaaaaaaaaaaaaaaa: ',name);
         console.log('other: ', this.myFormData?.additionalDetails[2]);
         this.buildForm([this.myFormData?.additionalDetails[2]]);
       }
       console.log(this.myForm);
-    } else {
+    }
+     else {
+      console.log("else kulla")
       this.myForm.removeControl('additionalDetails');
     }
   }
 
   onOthersDropdownChange(event: any, name: any) {
+    console.log('ingaaaaaaaaaaaaaaaaaaa: ',name,event.target.value);
+    if(this.idProof!==undefined && this.idProof!==event.target.value){
+      this.myForm.removeControl(this.idProof);
+    }
+    this.idProof=event.target.value
     // this.myForm.get(name)?.setValue(event.target.value);
     // console.log("jk: ",[this.myFormData?.additionalDetails[2]])
-    // this.buildForm([this.myFormData?.additionalDetails[2]]);
+    this.buildForm([this.myFormData?.additionalDetails[2]],this.idProof);
 
   }
 
